7385f9e111ce9e4c041d2701db3875ad169301f8
VieuxLoupGris <provocemois@gmail.com>
2019-02-12 16:14:55 +0100

    Not solve the chalenge

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ import 'bootstrap';
 */
 
 console.log("Hey look in your browser console. It works!");
+
+let photos = [];
+
+let cat1 = new Image();
+cat1.src = 'images/cat1.jpg';
+photos.push(cat1);
+
+let cat2 = new Image();
+cat2.src = 'images/cat2.jpeg';
+photos.push(cat2);
